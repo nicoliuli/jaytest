6e0ac8efbf5d098d1fb9ed8144e49fae87d4faf3
liuli <liuli@moqipobing.com>
2020-08-16 14:02:46 +0800

    1

--- src/main/java/com/jaytest/controller/TestController.java
+++ src/main/java/com/jaytest/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.jaytest.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.jaytest.model.ProjectResponse;
 import com.jaytest.model.RESUtils;
 import com.jaytest.model.TiRequest;
+import com.jaytest.model.TiResponse;
 import com.jaytest.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,9 +21,10 @@ public class TestController {
     @Autowired
     private TestService testService;
 
-    @PostMapping(value = "/ProjectMess1")
+    // 请求key=value，响应xml
+    @PostMapping(value = "/ProjectMess1",produces = MediaType.APPLICATION_XML_VALUE)
     @ResponseBody
-    public String ProjectMess(String requestString,String key) throws Exception {
+    public TiResponse ProjectMess(String requestString, String key) throws Exception {
         System.out.println(requestString);
         System.out.println(key);
 
@@ -31,7 +35,13 @@ public class TestController {
         log.info("解密后::" + rmdmess);
         log.info("json转成对象：{}",tiRequest);
         testService.insertTiRequest(tiRequest);
-       return "aaa";
+
+        TiResponse resp = new TiResponse();
+        ProjectResponse data = new ProjectResponse();
+        data.setCode("000");
+        data.setData("数据上报成功！");
+        resp.setDATA(data);
+       return resp;
     }
 
 }
